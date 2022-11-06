473af2beef7032d2f34a79127a8445dcc689423a
Aster <86864376+Stargazer-cyk@users.noreply.github.com>
2022-11-06 13:50:12 +0800

    Update dbdata.py

--- data/train/dbdata.py
+++ data/train/dbdata.py
@@ -2,21 +2,25 @@ import pandas as pd
 import numpy as np
 import math as mt
 from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
 data_path = r"C:\Users\22259\Desktop\chengdu80\UESTC\data\train\feature.csv"
 data = pd.read_csv(data_path)
 data = data.drop("APPLICATION_ID", axis=1)
 data = data.fillna(0)
+#print(data)
+new_data = preprocessing.normalize(data)
+#print(New_data)
 
-pca = PCA(n_components=1000)
-New_data = pca.fit_transform(data)
-New_data = np.transpose(New_data)
-db = DBSCAN(eps=0.05, min_samples=5)
-db.fit(New_data)
-ids = db.labels_
-mx = ids.max()
-print(mx)
-for i in range(1,mx):
+new_data = np.transpose(new_data)
+#db = DBSCAN(eps=0.5, min_samples=5,metric='manhattan')
+kmeans = KMeans(n_clusters=40, random_state=0).fit(new_data)
+ids = kmeans.labels_
+mx = np.max(ids)
+#fp=open(r'.\ids.txt', 'w')
+#print("end2")
+#print(ids,file=fp)
+for i in range(0,mx+1):
     print(np.sum(ids == i))
 print(np.sum(ids == -1))
 #print(ids)
