ef84d6eba6d523681c07353c9daabf64fc9c82a3
FFFcapatin328 <53992308+FFFcapatin328@users.noreply.github.com>
2022-11-06 21:10:39 +0800

    正负样本改1：3

--- data/data/featureMerge.py
+++ data/data/featureMerge.py
@@ -37,8 +37,8 @@ def ConstructDataset(FilePath, outputPath, clusterDf):
 
 originTrainPath = "./train/feature.csv"
 trainLablePath = "./train/label.csv"
-enhancedTrainPath = "./train/enhanced_feature.csv"
-enhancedTrainLabelPath = "./train/enhanced_feature_label.csv"
+enhancedTrainPath = "./train/enhanced_feature_3_1.csv"
+enhancedTrainLabelPath = "./train/enhanced_feature_label_3_1.csv"
 
 originTestPath = "./test/feature.csv"
 clusterPath = "./cluster_out.csv"
@@ -51,12 +51,12 @@ if not os.path.exists(enhancedTrainPath) and not os.path.exists(enhancedTrainLab
     print(Counter(df["DEFAULT_LABEL"]))
     trainDf = pd.read_csv(originTrainPath).drop("APPLICATION_ID", axis=1)
     trainDf = trainDf.fillna(0)
-    smo = SMOTE(random_state=111)
+    smo = SMOTE(random_state=111, sampling_strategy = {1:23985})
     newTrainDf, labelDf = smo.fit_resample(trainDf, df["DEFAULT_LABEL"])
     print(Counter(labelDf))
     print(newTrainDf)
     print(labelDf)
     newTrainDf.to_csv(enhancedTrainPath, index=False, sep=",")
     labelDf.to_csv(enhancedTrainLabelPath, index=False, sep=",")
-# ConstructDataset(enhancedTrainPath, constructedTrainPath, clusterDf)
+ConstructDataset(enhancedTrainPath, constructedTrainPath, clusterDf)
 # ConstructDataset(originTestPath, constructedTestPath, clusterDf)
