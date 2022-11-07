767f3ba7bb8b90683df8264a0f2670b62f8b4613
shallowmonologue <1054211027@qq.com>
2022-11-07 19:52:25 +0800

    HTML生成代码修改

--- UI/Config.py
+++ UI/Config.py
@@ -1,21 +1,39 @@
+# 获取当前绝对路径
 import os
+ABS_PATH = os.getcwd()
 
 # UI的配置设定文件
 WINDOW_WIDTH = 1800
 WINDOW_HEIGHT = 960
 
 # 主页的Web组件尺寸
-MAIN_WIDTH = '315px'
-MAIN_HEIGHT = '310px'
+MAIN_WIDTH = '315px'  # 325px
+MAIN_HEIGHT = '310px'  # 316px
+MAIN_WIDTH_ = '315px'  # 451px
+MAIN_HEIGHT_ = '310px'  # 361px
+
+# 用户页面的Web组件尺寸
+USER_WIDTH = '410px'  # 438px
+USER_HEIGHT = '340px'  # 365px
+USER_WIDTH_ = '950px'  # 971px
+USER_HEIGHT_ = '340px'  # 361px
+
+# 模型页面的Web组件尺寸
+MODEL_WIDTH = '800px'  # 826px
+MODEL_HEIGHT = '700px'  # 743px
+
+# 分析页面的Web组件尺寸
+ANALYSIS_WIDTH = '315px'
+ANALYSIS_HEIGHT = '310px'
+
 # 图表系列的Web组件尺寸
 WIDTH = '1300px'  # '1381px'
 HEIGHT = '700px'  # '741px'
 
-
-# 获取当前绝对路径
-ABS_PATH = os.getcwd()
-
 # Web组件存放路径
-HTML_PATH = './htmls/'
+PATH = './htmls/'
+MAIN_PATH = './htmls/main/'
+USER_PATH = './htmls/user/'
+MODEL_PATH = './htmls/model/'
+
 # HTML_PATH = './htmls_main/'
-MAIN_PATH = './htmls_main/'
--- UI/GenerateOther.py
+++ UI/GenerateOther.py
@@ -1,42 +1,14 @@
 from pyecharts import options as opts
-from pyecharts.charts import Liquid, MapGlobe
+from pyecharts.charts import Liquid
 from pyecharts.globals import SymbolType
-from pyecharts.faker import POPULATION
 from Config import *
-
-
-data = [x for _, x in POPULATION[1:]]
-low, high = min(data), max(data)
-
-# 生成地球
-c = (
-    MapGlobe()
-    .add_schema()
-    .add(
-        maptype="world",
-        series_name="World Population",
-        data_pair=POPULATION[1:],
-        is_map_symbol_show=False,
-        label_opts=opts.LabelOpts(is_show=False),
-    )
-    .set_global_opts(
-        visualmap_opts=opts.VisualMapOpts(
-            min_=low,
-            max_=high,
-            range_text=["max", "min"],
-            is_calculable=True,
-            range_color=["lightskyblue", "yellow", "orangered"],
-        )
-    )
-    .render("map_globe_base.html")
-)
 
 # LOGO动态效果
 def logo():
     liquid = Liquid(init_opts=opts.InitOpts(width='90px',height='90px'))
     liquid.add("lq", [0.3, 0.7], is_outline_show=False, shape=SymbolType.DIAMOND, color=[255,255,255])
     liquid.set_global_opts(title_opts=opts.TitleOpts(title=""))
-    liquid.render("./htmls_main/logo.html")
+    liquid.render("./main/logo.html")
 
 
 if __name__ == '__main__':
