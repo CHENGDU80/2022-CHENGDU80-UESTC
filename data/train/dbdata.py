afccd0a88a8155ae73fab26c68c311f2155fe712
Aster <86864376+Stargazer-cyk@users.noreply.github.com>
2022-11-07 11:36:02 +0800

    Update dbdata.py

--- data/train/dbdata.py
+++ data/train/dbdata.py
@@ -4,22 +4,29 @@ import math as mt
 from sklearn.cluster import DBSCAN
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
-data_path = r"C:\Users\22259\Desktop\chengdu80\UESTC\data\train\feature.csv"
-data = pd.read_csv(data_path)
-data = data.drop("APPLICATION_ID", axis=1)
-data = data.fillna(0)
+from sklearn.metrics.pairwise import cosine_similarity
+# data_path = r"feature.csv"
+# data = pd.read_csv(data_path)
+# data = data.drop("APPLICATION_ID", axis=1)
+# data = data.fillna(0)
 #print(data)
+data_path ='cos_data.csv'
+data=pd.read_csv(data_path)
 new_data = preprocessing.normalize(data)
-#print(New_data)
-
-new_data = np.transpose(new_data)
+new_data=np.transpose(new_data)
+# cos_data =cosine_similarity(new_data, new_data)
+# np.save(r"cos_data",cos_data)
+# exit()
+# #
+# new_data = np.transpose(new_data)
 #db = DBSCAN(eps=0.5, min_samples=5,metric='manhattan')
 kmeans = KMeans(n_clusters=40, random_state=0).fit(new_data)
 ids = kmeans.labels_
+
+#ids, error, nfound = kcluster(new_data,  nclusters=20, dist='u', npass=100)
 mx = np.max(ids)
-#fp=open(r'.\ids.txt', 'w')
-#print("end2")
-#print(ids,file=fp)
+fp=open('.\ids.txt', 'w')
+print(ids,file=fp)
 for i in range(0,mx+1):
     print(np.sum(ids == i))
 print(np.sum(ids == -1))
